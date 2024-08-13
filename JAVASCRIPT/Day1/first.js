443ea7a3030dd05e07d4269bc2e68bf9ff3c9c34
Sonia Sharma <Soniashar2021@gmail.com>
2024-08-13 22:10:17 +0530

    Create first.js

---
+++ JAVASCRIPT/Day1/first.js
@@ -0,0 +1,9 @@
+// console.log("This is my first JavaScript File")
+// console.log("Welcome Sonia to your new Journey ")
+// console.log("Welcome! Welcome!")
+
+fullName = "Sonia Sharma";
+console.log("fullName");
+
+age = 24;
+console.log("age");
