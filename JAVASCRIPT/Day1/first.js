3185a78203244d3bd5a6c0c536cddcddccdfd755
Sonia Sharma <Soniashar2021@gmail.com>
2024-08-17 12:54:56 +0530

    Update first.js

--- JAVASCRIPT/Day1/first.js
+++ JAVASCRIPT/Day1/first.js
@@ -2,18 +2,27 @@
 // console.log("Welcome Sonia to your new Journey ")
 // console.log("Welcome! Welcome!")
 
-fullName = "Sonia Sharma";
-age = 24;
-price = 99.99;
-radius = 14;
-a = null;
-y = undefined;
-isFollow = true;
+// fullName = "Sonia Sharma";
+// age = 24;
+// price = 99.99;
+// radius = 14;
+// a = null;
+// y = undefined;
+// isFollow = true;
 
-fullName = 25;
+// fullName = 25;
 
-console.log(fullName);
+// console.log(fullName);
 
+// let fullName = "Sonia Sharma";
+// let age = 24;
+// let totalPrice = 1000;
+
+// console.log(age);
+
+// 1
+let a = 5;
+console.log(a);
 
 
 
