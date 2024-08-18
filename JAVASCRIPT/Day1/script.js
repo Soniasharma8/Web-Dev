53a058013d7557759bca7ebba3ad86eb651e0527
Sonia Sharma <Soniashar2021@gmail.com>
2024-08-18 21:08:36 +0530

    Update script.js

---
+++ JAVASCRIPT/Day1/script.js
@@ -0,0 +1,39 @@
+//  Arithmetic Operators
+// let a = 5;
+// let b = 2;
+  
+// console.log("a =", a, " & b =", b);
+
+/*console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);*/
+
+//Unary Operator
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b = ", b);
+
+// a = a-1;
+// console.log("a++ = ", a++); //5
+// console.log("a = ", a); //6
+
+
+console.log("--a = ", --a); //4
+console.log("a = ", a); 
+
+
+
+
+
+
+
+
+
+
+
+
+
