f293e6b348dc1f11285db827a496cd5f5fc24e8d
Sonia Sharma <Soniashar2021@gmail.com>
2024-08-19 12:15:15 +0530

    Update script.js

--- JAVASCRIPT/Day1/script.js
+++ JAVASCRIPT/Day1/script.js
@@ -1,10 +1,9 @@
 //  Arithmetic Operators
-// let a = 5;
-// let b = 2;
-  
-// console.log("a =", a, " & b =", b);
 
-/*console.log("a + b =", a + b);
+ /*let a = 5;
+ let b = 2;
+console.log("a =", a, " & b =", b);
+console.log("a + b =", a + b);
 console.log("a - b =", a - b);
 console.log("a * b =", a * b);
 console.log("a / b =", a / b);
@@ -12,22 +11,68 @@ console.log("a % b =", a % b);
 console.log("a ** b =", a ** b);*/
 
 //Unary Operator
-let a = 5;
+
+/*let a = 5;
 let b = 2;
 
 console.log("a = ", a, "& b = ", b);
 
-// a = a-1;
-// console.log("a++ = ", a++); //5
-// console.log("a = ", a); //6
+a = a-1;
+console.log("a++ = ", a++); //5
+console.log("a = ", a); //6
 
 
 console.log("--a = ", --a); //4
-console.log("a = ", a); 
+console.log("a = ", a); */
 
 
+//Assignment Operators// 
 
+/* let a = 5;
+ let b = 2;
 
+ a += 4;  //a = a+4
+console.log("a = ", a);  //9 */
+
+// Comparison Operators
+
+/*let a = 5;
+let b = 2;
+ console.log("5 == 2", a == b);*/
+
+ //Logical Opeartors
+
+ /*let a = 6;
+ let b = 5;
+
+ let cond1 = a > b;
+ let cond2 = a === 6;
+ console.log("cond1 && cond2 = ", cond1 && cond2);*/
+
+//  conditional statements
+
+/*let age = 17;
+
+if (age >= 18) {
+    console.log("you can vote");
+}
+
+if (age < 18) {
+    console.log("You Cannot Vote");
+}*/
+
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}
+
+if (mode === "light") {
+    color = "white";
+}
+
+console.log(color);
 
 
 
